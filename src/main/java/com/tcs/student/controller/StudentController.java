ebc45cf3de709e844f315197e2ebfe267bda6285
unknown <deepakamboj@gmail.com>
2025-05-07 17:22:45 +0530

    The delete Api is created

--- src/main/java/com/tcs/student/controller/StudentController.java
+++ src/main/java/com/tcs/student/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.tcs.student.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class StudentController {
 
 	@Autowired
 	private StudentService studentService;
-	
+
 	@PostMapping("/create")
 	public Student create(@RequestBody Student student) {
 		Student createstudent = studentService.create(student);
@@ -35,6 +36,13 @@ public class StudentController {
 	@GetMapping("/get/{rollNo}")
 	public Student getById(@PathVariable int rollNo) {
 		return studentService.getById(rollNo);
+	}
+
+	@DeleteMapping("/delete/{rollNo}")
+	public String delete(@PathVariable int rollNo) {
+		studentService.delete(rollNo);
+		return "Record successfully deleted";
+
 	}
 
 }
